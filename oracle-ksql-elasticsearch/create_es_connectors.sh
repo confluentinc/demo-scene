cf455fd7c1ca9f8aa52908297c913b0343f551d3
Robin Moffatt <robin@confluent.io>
2018-05-16 13:54:35 +0100

    Demo as delivered at UKOUG Northern Tech Summit

---
+++ oracle-ksql-elasticsearch/create_es_connectors.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+curl -X "POST" "http://localhost:8083/connectors/" \
+     -H "Content-Type: application/json" \
+     -d '{
+  "name": "es_sink_LOGON_ENRICHED",
+  "config": {
+    "topics": "LOGON_ENRICHED",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+    "key.ignore": "true",
+    "schema.ignore": "false",
+    "type.name": "type.name=kafkaconnect",
+    "topic.index.map": "LOGON_ENRICHED:'logon_enriched'",
+    "connection.url": "http://localhost:9200"
+  }
+}'
+
+curl -X "POST" "http://localhost:8083/connectors/" \
+     -H "Content-Type: application/json" \
+     -d '{
+  "name": "es_sink_ora-soe-ORDERS",
+  "config": {
+    "topics": "ora-soe-ORDERS",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+    "key.ignore": "true",
+    "schema.ignore": "false",
+    "type.name": "type.name=kafkaconnect",
+    "topic.index.map": "ora-soe-ORDERS:'orders'",
+    "connection.url": "http://localhost:9200"
+  }
+}'
+
