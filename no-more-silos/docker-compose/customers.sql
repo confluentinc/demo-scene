840b3c072d46932fa4b45234df34e34e7abc868e
Robin Moffatt <robin@confluent.io>
2018-06-04 16:12:09 +0100

    Debezium works

--- no-more-silos/docker-compose/customers.sql
+++ no-more-silos/docker-compose/customers.sql
@@ -7,7 +7,7 @@ GRANT ALL PRIVILEGES ON demo.* TO 'mysqluser'@'%';
 
 use demo;
 
-create table customers (
+create table CUSTOMERS (
 	id INT PRIMARY KEY,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
