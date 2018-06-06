69e48f3b8cd32098625d81a3c9b497dde014b107
Robin Moffatt <robin@confluent.io>
2018-06-06 16:09:34 +0100

    MySQL/KSQL/ES demo

--- mysql-debezium-ksql-elasticsearch/add-es-connector.sh
+++ mysql-debezium-ksql-elasticsearch/add-es-connector.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -X "POST" "http://localhost:8083/connectors/" \
+curl -X "POST" "http://localhost:18083/connectors/" \
      -H "Content-Type: application/json" \
      -d '{
   "name": "es_sink_RATINGS_ENRICHED",
@@ -12,7 +12,7 @@ curl -X "POST" "http://localhost:8083/connectors/" \
     "schema.ignore": "true",
     "type.name": "type.name=kafkaconnect",
     "topic.index.map": "'RATINGS_ENRICHED':'ratings_enriched'",
-    "connection.url": "http://localhost:9200",
+    "connection.url": "http://elasticsearch:9200",
     "transforms": "ExtractTimestamp",
     "transforms.ExtractTimestamp.type": "org.apache.kafka.connect.transforms.InsertField$Value",
     "transforms.ExtractTimestamp.timestamp.field" : "EXTRACT_TS"
--- mysql-debezium-ksql-elasticsearch/docker-compose/customers.sql
+++ mysql-debezium-ksql-elasticsearch/docker-compose/customers.sql
@@ -1,18 +1,27 @@
+GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator' IDENTIFIED BY 'replpass';
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'debezium' IDENTIFIED BY 'dbz';
+
+# Create the database that we'll use to populate data and watch the effect in the binlog
+CREATE DATABASE demo;
+GRANT ALL PRIVILEGES ON demo.* TO 'mysqluser'@'%';
+
 use demo;
-drop table customers;
-create table customers (
+
+create table CUSTOMERS (
 	id INT PRIMARY KEY,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
 	email VARCHAR(50),
 	gender VARCHAR(50),
-	comments VARCHAR(90)
+	comments VARCHAR(90),
+	create_ts timestamp DEFAULT CURRENT_TIMESTAMP ,
+	update_ts timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-insert into customers (id, first_name, last_name, email, gender, comments) values (1, 'Bibby', 'Argabrite', 'bargabrite0@google.com.hk', 'Female', 'Reactive exuding productivity');
-insert into customers (id, first_name, last_name, email, gender, comments) values (2, 'Auberon', 'Sulland', 'asulland1@slideshare.net', 'Male', 'Organized context-sensitive Graphical User Interface');
-insert into customers (id, first_name, last_name, email, gender, comments) values (3, 'Marv', 'Dalrymple', 'mdalrymple2@macromedia.com', 'Male', 'Versatile didactic pricing structure');
-insert into customers (id, first_name, last_name, email, gender, comments) values (4, 'Nolana', 'Yeeles', 'nyeeles3@drupal.org', 'Female', 'Adaptive real-time archive');
-insert into customers (id, first_name, last_name, email, gender, comments) values (5, 'Modestia', 'Coltart', 'mcoltart4@scribd.com', 'Female', 'Reverse-engineered non-volatile success');
-insert into customers (id, first_name, last_name, email, gender, comments) values (6, 'Bram', 'Acaster', 'bacaster5@pagesperso-orange.fr', 'Male', 'Robust systematic support');
-insert into customers (id, first_name, last_name, email, gender, comments) values (7, 'Marigold', 'Veld', 'mveld6@pinterest.com', 'Female', 'Sharable logistical installation');
-insert into customers (id, first_name, last_name, email, gender, comments) values (8, 'Ruperto', 'Matteotti', 'rmatteotti7@diigo.com', 'Male', 'Diverse client-server conglomeration');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (1, 'Bibby', 'Argabrite', 'bargabrite0@google.com.hk', 'Female', 'Reactive exuding productivity');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (2, 'Auberon', 'Sulland', 'asulland1@slideshare.net', 'Male', 'Organized context-sensitive Graphical User Interface');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (3, 'Marv', 'Dalrymple', 'mdalrymple2@macromedia.com', 'Male', 'Versatile didactic pricing structure');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (4, 'Nolana', 'Yeeles', 'nyeeles3@drupal.org', 'Female', 'Adaptive real-time archive');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (5, 'Modestia', 'Coltart', 'mcoltart4@scribd.com', 'Female', 'Reverse-engineered non-volatile success');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (6, 'Bram', 'Acaster', 'bacaster5@pagesperso-orange.fr', 'Male', 'Robust systematic support');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (7, 'Marigold', 'Veld', 'mveld6@pinterest.com', 'Female', 'Sharable logistical installation');
+insert into CUSTOMERS (id, first_name, last_name, email, gender, comments) values (8, 'Ruperto', 'Matteotti', 'rmatteotti7@diigo.com', 'Male', 'Diverse client-server conglomeration');
