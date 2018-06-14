7ff1bd1566f8409f792afcec59483033f0b5a600
Robin Moffatt <robin@confluent.io>
2018-06-14 18:37:48 +0200

    Updated for new stream name, add KSQL statements, updated Kibana viz

--- mysql-debezium-ksql-elasticsearch/add-es-connector.sh
+++ mysql-debezium-ksql-elasticsearch/add-es-connector.sh
@@ -3,15 +3,35 @@
 curl -X "POST" "http://localhost:18083/connectors/" \
      -H "Content-Type: application/json" \
      -d '{
-  "name": "es_sink_RATINGS_ENRICHED",
+  "name": "es_sink_unhappy_platinum_customers",
   "config": {
-    "topics": "'RATINGS_ENRICHED'",
+    "topics": "UNHAPPY_PLATINUM_CUSTOMERS",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+	"value.converter": "org.apache.kafka.connect.json.JsonConverter",
+	"value.converter.schemas.enable": false,
+    "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+    "key.ignore": "true",
+    "schema.ignore": "true",
+    "type.name": "type.name=kafkaconnect",
+    "topic.index.map": "UNHAPPY_PLATINUM_CUSTOMERS:unhappy_platinum_customers",
+    "connection.url": "http://elasticsearch:9200",
+    "transforms": "ExtractTimestamp",
+    "transforms.ExtractTimestamp.type": "org.apache.kafka.connect.transforms.InsertField$Value",
+    "transforms.ExtractTimestamp.timestamp.field" : "EXTRACT_TS"
+  }
+}'
+
+curl -X "POST" "http://localhost:18083/connectors/" \
+     -H "Content-Type: application/json" \
+     -d '{
+  "name": "es_sink_ratings-with-customer-data",
+  "config": {
+    "topics": "ratings-with-customer-data",
     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "key.ignore": "true",
     "schema.ignore": "true",
     "type.name": "type.name=kafkaconnect",
-    "topic.index.map": "'RATINGS_ENRICHED':'ratings_enriched'",
     "connection.url": "http://elasticsearch:9200",
     "transforms": "ExtractTimestamp",
     "transforms.ExtractTimestamp.type": "org.apache.kafka.connect.transforms.InsertField$Value",
