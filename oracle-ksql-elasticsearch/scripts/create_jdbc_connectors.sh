daf3ac04da78b2eac66ed7402945eaf0ffce33d0
Robin Moffatt <robin@confluent.io>
2018-06-25 09:59:15 +0100

    WIP Oracle demo

--- oracle-ksql-elasticsearch/scripts/create_jdbc_connectors.sh
+++ oracle-ksql-elasticsearch/scripts/create_jdbc_connectors.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 curl -i -X POST -H "Accept:application/json" \
-  -H  "Content-Type:application/json" http://localhost:8083/connectors/ \
+  -H  "Content-Type:application/json" http://localhost:18083/connectors/ \
   -d '{
         "name": "jdbc_source_oracle_soe_logon",
         "config": {
                 "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-                "connection.url": "jdbc:oracle:thin:soe/soe@localhost:1521/ORCLPDB1",
+                "connection.url": "jdbc:oracle:thin:soe/soe@oracle:1521/ORCLPDB1",
                 "mode": "incrementing",
                 "query": "SELECT CAST(LOGON_ID AS NUMERIC(8,0)) AS LOGON_ID, CAST(CUSTOMER_ID AS NUMERIC(18,0)) AS CUSTOMER_ID, LOGON_DATE FROM LOGON",
                 "poll.interval.ms": "1000",
@@ -23,12 +23,12 @@ curl -i -X POST -H "Accept:application/json" \
 }'
 
 curl -i -X POST -H "Accept:application/json" \
-  -H  "Content-Type:application/json" http://localhost:8083/connectors/ \
+  -H  "Content-Type:application/json" http://localhost:18083/connectors/ \
   -d '{
         "name": "jdbc_source_oracle_soe_orders",
         "config": {
                 "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-                "connection.url": "jdbc:oracle:thin:soe/soe@localhost:1521/ORCLPDB1",
+                "connection.url": "jdbc:oracle:thin:soe/soe@oracle:1521/ORCLPDB1",
                 "query":"SELECT ORDER_ID, CAST(ORDER_DATE AS TIMESTAMP) AS ORDER_DATE, ORDER_MODE, CUSTOMER_ID, ORDER_STATUS, ORDER_TOTAL, SALES_REP_ID, PROMOTION_ID, WAREHOUSE_ID, DELIVERY_TYPE, COST_OF_DELIVERY, WAIT_TILL_ALL_AVAILABLE, DELIVERY_ADDRESS_ID, CUSTOMER_CLASS, CARD_ID, INVOICE_ADDRESS_ID FROM ORDERS",
                 "mode": "incrementing",
                 "poll.interval.ms": "1000",
@@ -45,12 +45,12 @@ curl -i -X POST -H "Accept:application/json" \
 }'
 
 curl -i -X POST -H "Accept:application/json" \
-  -H  "Content-Type:application/json" http://localhost:8083/connectors/ \
+  -H  "Content-Type:application/json" http://localhost:18083/connectors/ \
   -d '{
         "name": "jdbc_source_oracle_soe_customers",
         "config": {
                 "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-                "connection.url": "jdbc:oracle:thin:soe/soe@localhost:1521/ORCLPDB1",
+                "connection.url": "jdbc:oracle:thin:soe/soe@oracle:1521/ORCLPDB1",
                 "table.whitelist":"CUSTOMERS",
                 "mode": "incrementing",
                 "poll.interval.ms": "1000",
