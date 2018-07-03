8294c2689ae54a4488b7d60dff74f6bfbc6b236c
Robin Moffatt <robin@confluent.io>
2018-07-03 08:45:18 +0100

    Update demo, create short version

--- postgres-debezium-ksql-elasticsearch/docker-compose/scripts/create-pg-sink.sh
+++ postgres-debezium-ksql-elasticsearch/docker-compose/scripts/create-pg-sink.sh
@@ -6,8 +6,13 @@ curl -X "POST" "http://localhost:18083/connectors/" \
              "name": "jdbc_sink_postgres",
              "config": {
                  "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
-                 "connection.url": "jdbc:postgresql://postgres:5432/database?user=postgres&password=postgres",
+                 "connection.url": "jdbc:postgresql://postgres:5432/postgres?user=postgres&password=postgres",
                  "auto.create":"true",
-                 "topics":"ratings, UNHAPPY_PLATINUM_CUSTOMERS"
+                 "auto.evolve":"true",
+                 "topics": "RATINGS_WITH_CUSTOMER_DATA",
+                 "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                 "transforms": "ExtractTimestamp",
+                 "transforms.ExtractTimestamp.type": "org.apache.kafka.connect.transforms.InsertField$Value",
+                 "transforms.ExtractTimestamp.timestamp.field" : "EXTRACT_TS"
              }
      }'
