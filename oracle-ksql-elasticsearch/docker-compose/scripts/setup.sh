df3e4a7fa8f5dbca613d38aed5e4a6fd0a6f1ff6
Robin Moffatt <robin@confluent.io>
2018-11-30 12:41:21 +0000

    Updated startup script

--- oracle-ksql-elasticsearch/docker-compose/scripts/setup.sh
+++ oracle-ksql-elasticsearch/docker-compose/scripts/setup.sh
@@ -3,10 +3,13 @@
 echo -e "Firing up Docker Compose"
 docker-compose up -d
 
+echo -e "\n--\n\nWaiting for Oracle to be available … ⏳"
+grep -q "DATABASE IS READY TO USE!" <(docker-compose logs -f oracle)
+
 export CONNECT_HOST=connect-debezium
 echo -e "\n--\n\nWaiting for Kafka Connect to start on $CONNECT_HOST … ⏳"
 grep -q "Kafka Connect started" <(docker-compose logs -f $CONNECT_HOST)
-echo -e "\n--\n+> Creating Kafka Connect MySQL Debezium source"
+echo -e "\n--\n+> Creating Kafka Connect Oracle Debezium source"
 docker-compose exec connect-debezium bash -c '/scripts/create-ora-source.sh'
 
 echo -e "\n--\n\nWaiting for elasticsearch to start "
