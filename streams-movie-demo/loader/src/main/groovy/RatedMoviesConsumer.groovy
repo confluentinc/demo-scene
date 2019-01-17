2dd62abbefc80597a517d079e3b8ffa0fa813d60
Viktor Gamov <viktor@confluent.io>
2019-01-17 13:40:04 -0500

    Refactored groovy examples

--- streams-movie-demo/loader/src/main/groovy/RatedMoviesConsumer.groovy
+++ streams-movie-demo/loader/src/main/groovy/RatedMoviesConsumer.groovy
@@ -1,29 +1,31 @@
 import groovy.transform.CompileStatic
-import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.LongDeserializer
+import org.apache.kafka.common.serialization.StringDeserializer
 
 import static java.time.Duration.ofMillis
-import static java.util.Arrays.asList
 import static org.apache.kafka.clients.consumer.ConsumerConfig.*
 
 @CompileStatic
 class RatedMoviesConsumer {
-  public static final String topicName = "rated-movies"
+
+  static final def topicName = 'rated-movies'
 
   static void main(args) {
 
-    Properties p = new Properties()
-    p.put(BOOTSTRAP_SERVERS_CONFIG, 'localhost:9092')
-    p.put(GROUP_ID_CONFIG, 'rated-movies-groovy-consumer')
-    p.put(KEY_DESERIALIZER_CLASS_CONFIG, 'org.apache.kafka.common.serialization.LongDeserializer')
-    p.put(VALUE_DESERIALIZER_CLASS_CONFIG, 'org.apache.kafka.common.serialization.StringDeserializer')
+    def p = new Properties()
+    p[BOOTSTRAP_SERVERS_CONFIG] = 'localhost:9092'
+    p[GROUP_ID_CONFIG] = 'rated-movies-groovy-consumer'
+    p[KEY_DESERIALIZER_CLASS_CONFIG] = LongDeserializer.name
+    p[VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer.name
 
-    KafkaConsumer consumer = new KafkaConsumer(p)
-    consumer.subscribe(asList(topicName))
+    def consumer = new KafkaConsumer(p)
+    consumer.subscribe([topicName] as List<String>)
 
     while (true) {
-      ConsumerRecords records = consumer.poll(ofMillis(100))
-      for (it in records) {
+      def records = consumer.poll(ofMillis(100))
+      records.forEach { ConsumerRecord it ->
         println it.value()
       }
     }
