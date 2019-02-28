ef84c615ffd8c064d06f582e68f7f093d193dfbb
Viktor Gamov <viktor@confluent.io>
2019-02-28 17:16:15 -0500

    --wip-- [skip ci]

--- streams-movie-demo/data/reset-raw.sh
+++ streams-movie-demo/data/reset-raw.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-confluent destroy
-confluent start schema-registry
+# confluent destroy
+# confluent start schema-registry
 cat movies.dat| kafka-console-producer --broker-list localhost:9092 --topic raw-movies
 cat ratings.dat | kafka-console-producer --broker-list localhost:9092 --topic raw-ratings
 # enable compaction for this topics
---
+++ streams-movie-demo/kotlin-streams/src/main/kotlin/io/confluent/demo/util/Extentions.kt
@@ -0,0 +1,1 @@
+package io.confluent.demo.util
