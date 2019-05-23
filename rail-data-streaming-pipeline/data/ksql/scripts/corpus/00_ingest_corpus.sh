0ecd397ab994a8dd41a7e1eb3f66b80f9636c6b9
Robin Moffatt <robin.moffatt@rittmanmead.com>
2019-05-23 16:29:00 +0100

    Update corpus ingest to use env var for username and password, add sample data

--- rail-data-streaming-pipeline/data/ksql/scripts/corpus/00_ingest_corpus.sh
+++ rail-data-streaming-pipeline/data/ksql/scripts/corpus/00_ingest_corpus.sh
@@ -3,7 +3,7 @@
 # This uses jq to explode the source array into individual 
 # messages, since KSQL can't do that yet
 
-curl -s -L -u "NROD_USERNAME:NROD_PASSWORD" http://datafeeds.networkrail.co.uk/ntrod/SupportingFileAuthenticate?type=CORPUS | \
+curl -s -L -u "$NROD_USERNAME:$NROD_PASSWORD" http://datafeeds.networkrail.co.uk/ntrod/SupportingFileAuthenticate?type=CORPUS | \
   gunzip | \
   jq -c '.[][]' | \
   kafkacat -b localhost -P -t corpus
