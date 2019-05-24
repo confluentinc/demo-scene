c2567502e9cce85fbc6ee5b81570a00a1f9ed9b7
Robin Moffatt <robin.moffatt@rittmanmead.com>
2019-05-24 11:21:01 +0100

    Rearrange folders

--- rail-data-streaming-pipeline/data/ingest/cif_schedule/00_ingest_schedule.sh
+++ rail-data-streaming-pipeline/data/ingest/cif_schedule/00_ingest_schedule.sh
@@ -4,11 +4,11 @@
 # https://datafeeds.networkrail.co.uk/ntrod/myFeeds you are subscribed
 # to the appropriate feed for the Train Operating Company (TOC) for 
 # which you are pulling the schedule data
-curl -s -L -u "NROD_USERNAME:NROD_PASSWORD" "https://datafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_EA_TOC_FULL_DAILY&day=toc-full" | \
+curl -s -L -u "$NROD_USERNAME:$NROD_PASSWORD" "https://datafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_EA_TOC_FULL_DAILY&day=toc-full" | \
   gunzip | \
   kafkacat -b localhost -P -t CIF_FULL_DAILY
 
-curl -s -L -u "NROD_USERNAME:NROD_PASSWORD" "https://datafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_ED_TOC_FULL_DAILY&day=toc-full" | \
+curl -s -L -u "$NROD_USERNAME:$NROD_PASSWORD" "https://datafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_ED_TOC_FULL_DAILY&day=toc-full" | \
   gunzip | \
   kafkacat -b localhost -P -t CIF_FULL_DAILY
 
