892e703998f96dada4399cb3e9095bedec7c0eeb
Robin Moffatt <robin@confluent.io>
2019-06-07 17:42:01 +0100

    Add composite key for TMAS

--- rail-data-streaming-pipeline/data/egress/sink_es.sh
+++ rail-data-streaming-pipeline/data/egress/sink_es.sh
@@ -68,7 +68,28 @@ curl -i -X POST -H "Accept:application/json" \
   }
 }'
 
-
+curl -i -X POST -H "Accept:application/json" \
+    -H  "Content-Type:application/json" http://localhost:8083/connectors/ \
+    -d '{
+  "name": "sink-elastic-train-movements-v01",
+  "config": {
+    "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+    "topics": "TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_01",
+    "connection.url": "http://elasticsearch:9200",
+    "type.name": "type.name=kafkaconnect",
+    "key.ignore": "false",
+    "schema.ignore": "true",
+    "transforms": "ValueToKey,extractKey,changeTopic",
+    "transforms.ValueToKey.type":"org.apache.kafka.connect.transforms.ValueToKey",
+    "transforms.ValueToKey.fields":"MSG_KEY",
+    "transforms.extractKey.type":"org.apache.kafka.connect.transforms.ExtractField$Key",
+    "transforms.extractKey.field":"MSG_KEY"    
+    "transforms.changeTopic.type":"org.apache.kafka.connect.transforms.RegexRouter",
+    "transforms.changeTopic.regex":"TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_00",
+    "transforms.changeTopic.replacement":"train-movements",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter"
+  }
+}'
 curl -i -X POST -H "Accept:application/json" \
     -H  "Content-Type:application/json" http://localhost:8083/connectors/ \
     -d '{
