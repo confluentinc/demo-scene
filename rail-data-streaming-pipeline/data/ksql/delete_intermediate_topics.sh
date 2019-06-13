5ba592cf209bee630d0c315e8ad87298704767df
Robin Moffatt <robin@rmoff.net>
2019-06-13 14:35:11 +0100

    Don't delete source topic you idiot

--- rail-data-streaming-pipeline/data/ksql/delete_intermediate_topics.sh
+++ rail-data-streaming-pipeline/data/ksql/delete_intermediate_topics.sh
@@ -15,8 +15,8 @@ docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --top
 docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --topic TRAIN_MOVEMENTS_ACTIVATIONS_00
 docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --topic TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_00
 docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --topic TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_01
+docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --topic TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_03
 docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --topic canx_reason_code2
-docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --topic networkrail_TRAIN_MVT
 docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --delete --topic schedule
 curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/CORPUS_BY_STANOX-value
 curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/SCHEDULE_00-value
@@ -31,3 +31,5 @@ curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/TRAIN_MOVEMENTS_00-valu
 curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/TRAIN_MOVEMENTS_01-value
 curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/TRAIN_MOVEMENTS_ACTIVATIONS_00-value
 curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_00-value
+curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_01-value
+curl -X DELETE http://proxmox01.moffatt.me:8081/subjects/TRAIN_MOVEMENTS_ACTIVATIONS_SCHEDULE_03-value
