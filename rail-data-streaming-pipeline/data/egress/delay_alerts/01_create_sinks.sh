47ded17a7646477ab27f6a007b0c89768a79f63b
Robin Moffatt <robin@confluent.io>
2019-06-21 10:22:44 +0100

    Update alerts

--- rail-data-streaming-pipeline/data/egress/delay_alerts/01_create_sinks.sh
+++ rail-data-streaming-pipeline/data/egress/delay_alerts/01_create_sinks.sh
@@ -1,14 +1,14 @@
 curl -X POST -H "Content-Type: application/json" http://localhost:38083/connectors/ \
       -d '{
-            "name": "sink-http-telegram-delay-alerts-02",
+            "name": "sink-http-telegram-delay-alerts-04",
             "config": {
               "connector.class": "io.confluent.connect.http.HttpSinkConnector",
               "request.method": "post",
               "http.api.url": "https://api.telegram.org/bot${file:/data/credentials.properties:TELEGRAM_BOT_API_KEY}/sendMessage",
               "headers": "Content-Type: application/json",
-              "topics": "TRAINS_DELAYED_ALERT",
+              "topics": "TRAINS_DELAYED_ALERT_TG",
               "tasks.max": "1",
-              "batch.prefix": "{\"chat_id\":\"-364377679\",",
+              "batch.prefix": "{\"chat_id\":\"-364377679\",\"parse_mode\": \"markdown\",",
               "batch.suffix": "}",
               "batch.max.size": "1",
               "regex.patterns":".*\\\{MESSAGE=(.*)\\\}.*",
