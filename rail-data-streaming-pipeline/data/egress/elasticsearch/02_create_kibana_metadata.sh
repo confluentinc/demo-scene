f606f9ea304e1a52b90acbf860b0838c4dcd0158
Robin Moffatt <robin@confluent.io>
2019-09-05 10:41:32 +0100

    set x

--- rail-data-streaming-pipeline/data/egress/elasticsearch/02_create_kibana_metadata.sh
+++ rail-data-streaming-pipeline/data/egress/elasticsearch/02_create_kibana_metadata.sh
@@ -2,22 +2,22 @@ echo "Create index patterns"
 curl -XPOST 'http://localhost:5601/api/saved_objects/index-pattern/train_cancellations_activations_schedule_00' \
     -H 'kbn-xsrf: nevergonnagiveyouup' \
     -H 'Content-Type: application/json' \
-    -d '{"attributes":{"title":"train_cancellations_activations_schedule_00","timeFieldName":"CREATE_TS"}}'
+    -d '{"attributes":{"title":"train_cancellations_activations_schedule_00","timeFieldName":"CANX_TIMESTAMP"}}'
 
 curl -XPOST 'http://localhost:5601/api/saved_objects/index-pattern/train_movements_activations_schedule_00' \
     -H 'kbn-xsrf: nevergonnagiveyouup' \
     -H 'Content-Type: application/json' \
-    -d '{"attributes":{"title":"train_movements_activations_schedule_00","timeFieldName":"CREATE_TS"}}'
+    -d '{"attributes":{"title":"train_movements_activations_schedule_00","timeFieldName":"ACTUAL_TIMESTAMP"}}'
 
 curl -XPOST 'http://localhost:5601/api/saved_objects/index-pattern/train_cancellations_02' \
     -H 'kbn-xsrf: nevergonnagiveyouup' \
     -H 'Content-Type: application/json' \
-    -d '{"attributes":{"title":"train_cancellations_02","timeFieldName":"CREATE_TS"}}'
+    -d '{"attributes":{"title":"train_cancellations_02","timeFieldName":"CANX_TIMESTAMP"}}'
 
 curl -XPOST 'http://localhost:5601/api/saved_objects/index-pattern/train_movements_01' \
     -H 'kbn-xsrf: nevergonnagiveyouup' \
     -H 'Content-Type: application/json' \
-    -d '{"attributes":{"title":"train_movements_01","timeFieldName":"CREATE_TS"}}'
+    -d '{"attributes":{"title":"train_movements_01","timeFieldName":"ACTUAL_TIMESTAMP"}}'
 
 echo "Setting the index pattern as default"
 curl -XPOST 'http://localhost:5601/api/kibana/settings' \
@@ -30,4 +30,3 @@ curl -XPOST 'http://localhost:5601/api/telemetry/v1/optIn' \
     -H 'kbn-xsrf: nevergonnagiveyouup' \
     -H 'content-type: application/json' \
     -d '{"enabled":false}'
-
