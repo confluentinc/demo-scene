0cb5ca1cdecdbc6a34f719896f737d8906659966
Robin Moffatt <robin@confluent.io>
2019-09-05 10:10:06 +0100

    Don't echo kafkacat processing to stdout

--- rail-data-streaming-pipeline/data/ingest/cif_schedule/01_process_schedule.sh
+++ rail-data-streaming-pipeline/data/ingest/cif_schedule/01_process_schedule.sh
@@ -4,4 +4,4 @@
 #kafkacat -b localhost:9092 -e -t CIF_FULL_DAILY -o beginning | \
 kafkacat -b localhost:9092 -e -u -G 00_ingest_schedule.sh CIF_FULL_DAILY | \
   jq -c '.|select(.JsonScheduleV1) | .JsonScheduleV1 + {last_schedule_segment:.JsonScheduleV1.schedule_segment.schedule_location[-1:][]}' | \
-  kafkacat -b localhost:9092 -t JsonScheduleV1 -T -P
+  kafkacat -b localhost:9092 -t JsonScheduleV1 -P
