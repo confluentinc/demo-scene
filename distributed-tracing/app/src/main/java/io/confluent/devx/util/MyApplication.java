07123aa2ae3401fe35d39065813ddc17f49605a1
Viktor Gamov <viktor@confluent.io>
2019-09-13 19:45:44 -0400

    Refactored app, added basic automation, skeleton readme

--- distributed-tracing/app/src/main/java/io/confluent/devx/util/MyApplication.java
+++ distributed-tracing/app/src/main/java/io/confluent/devx/util/MyApplication.java
@@ -1,7 +1,9 @@
 package io.confluent.devx.util;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -10,5 +12,11 @@ public class MyApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MyApplication.class, args);
+  }
+
+  @Bean
+  // kafka-topics --bootstrap-server localhost:9092 --create --topic STAGE1 --partitions 4 --replication-factor 1
+  NewTopic newTopic() {
+    return new NewTopic("STAGE1", 4, (short) 1);
   }
 }
