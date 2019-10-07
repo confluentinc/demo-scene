e6c2144a405c7120afe14c3dfc9d2803459f5d90
Robin Moffatt <robin@confluent.io>
2019-10-07 15:58:36 +0100

    Bumped and tested ES7

--- mqtt-tracker/load_sample_data.sh
+++ mqtt-tracker/load_sample_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-kafkacat -b localhost:9092 -t data_mqtt-import -K: -P -T -l ./data/export_20190929.kcat 
+kafkacat -b localhost:9092 -t data_mqtt-import -K: -P -T -l ./data/dummy_data.kcat
