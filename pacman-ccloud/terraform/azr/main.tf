a0a96b48d28c03b39d58d834c450866823fd36cc
riferrei <riferrei@riferrei.net>
2019-10-09 11:27:27 -0400

    Fixed identing in Azure impl

--- pacman-ccloud/terraform/azr/main.tf
+++ pacman-ccloud/terraform/azr/main.tf
@@ -3,18 +3,18 @@
 ###########################################
 
 provider "azurerm" {
-    subscription_id = var.azure_subscription_id
-    client_id = var.azure_client_id
-    client_secret = var.azure_client_secret
-    tenant_id = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant_id
 }
 
 resource "azurerm_resource_group" "azure_resource_group" {
-    name = var.global_prefix
-    location = local.region
-    tags = {
-        environment = var.global_prefix
-    }
+  name = var.global_prefix
+  location = local.region
+  tags = {
+    environment = var.global_prefix
+  }
 }
 
 variable "azure_subscription_id" {
--- pacman-ccloud/terraform/azr/networking.tf
+++ pacman-ccloud/terraform/azr/networking.tf
@@ -3,9 +3,9 @@
 ###########################################
 
 resource "azurerm_virtual_network" "default" {
-  name                = var.global_prefix
-  address_space       = ["10.0.0.0/16"]
-  location            = local.region
+  name = var.global_prefix
+  address_space = ["10.0.0.0/16"]
+  location = local.region
   resource_group_name = azurerm_resource_group.azure_resource_group.name
   tags = {
     environment = var.global_prefix
@@ -17,15 +17,15 @@ resource "azurerm_virtual_network" "default" {
 ###########################################
 
 resource "azurerm_subnet" "private_subnet" {
-  name                 = "private-subnet-${var.global_prefix}"
+  name = "private-subnet-${var.global_prefix}"
   resource_group_name  = azurerm_resource_group.azure_resource_group.name
   virtual_network_name = azurerm_virtual_network.default.name
-  address_prefix       = "10.0.1.0/24"
+  address_prefix = "10.0.1.0/24"
 }
 
 resource "azurerm_subnet" "public_subnet" {
-  name                 = "public-subnet-${var.global_prefix}"
-  resource_group_name  = azurerm_resource_group.azure_resource_group.name
+  name = "public-subnet-${var.global_prefix}"
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
   virtual_network_name = azurerm_virtual_network.default.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefix = "10.0.2.0/24"
 }
