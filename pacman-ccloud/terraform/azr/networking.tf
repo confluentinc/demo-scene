7db0a2122fa41bf2b8275fa3436c7eeff2962ba5
riferrei <riferrei@riferrei.net>
2019-10-10 19:42:27 -0400

    Added support of security groups

--- pacman-ccloud/terraform/azr/networking.tf
+++ pacman-ccloud/terraform/azr/networking.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "default" {
 ###########################################
 
 resource "azurerm_subnet" "private_subnet" {
-  name = "private-subnet-${var.global_prefix}"
+  name = "${var.global_prefix}-private-subnet"
   resource_group_name  = azurerm_resource_group.azure_resource_group.name
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefix = "10.0.1.0/24"
@@ -49,4 +49,52 @@ resource "azurerm_public_ip" "bastion_server" {
   domain_name_label = "pacman${random_string.random_string.result}-ssh"
   resource_group_name = azurerm_resource_group.azure_resource_group.name
   allocation_method = "Static"
+}
+
+###########################################
+############# Security Groups #############
+###########################################
+
+resource "azurerm_network_security_group" "private_subnet" {
+  name = "${var.global_prefix}-private-subnet"
+  location = local.region
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+  security_rule {
+    name                       = "inbound-bastion-server"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "inbound-rest-proxy"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8082"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "inbound-ksql-server"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8088"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "private_subnet" {
+  subnet_id = azurerm_subnet.private_subnet.id
+  network_security_group_id = azurerm_network_security_group.private_subnet.id
 }
