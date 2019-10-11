1180cee1a5c367b456cd0667c57064596f0fcab9
riferrei <riferrei@riferrei.net>
2019-10-11 16:38:54 -0400

    Added Azure support for README

--- pacman-ccloud/terraform/azr/outputs.tf
+++ pacman-ccloud/terraform/azr/outputs.tf
@@ -6,14 +6,18 @@ output "Pacman" {
   value = replace(azurerm_storage_account.pacman.primary_web_endpoint, "https", "http")
 }
 
-output "REST_Proxy" {
-  value = var.instance_count["rest_proxy"] >= 1 ? "http://${azurerm_public_ip.rest_proxy[0].fqdn}" : "REST Proxy has been disabled"
-}
-
 output "KSQL_Server" {
   value = var.instance_count["ksql_server"] >= 1 ? "http://${azurerm_public_ip.ksql_server[0].fqdn}" : "KSQL Server has been disabled"
 }
 
+/************** Uncomment this if you need access to the REST Proxy endpoint **************
+output "REST_Proxy" {
+  value = var.instance_count["rest_proxy"] >= 1 ? "http://${azurerm_public_ip.rest_proxy[0].fqdn}" : "REST Proxy has been disabled"
+}
+*/
+
+/************** Uncomment this if you need access to the Bastion Server *******************
 output "Bastion_Server" {
   value = var.instance_count["bastion_server"] >= 1 ? "ssh azure@${azurerm_public_ip.bastion_server[0].fqdn} -i cert.pem" : "Bastion Server has been disabled"
 }
+*/
--- pacman-ccloud/terraform/gcp/outputs.tf
+++ pacman-ccloud/terraform/gcp/outputs.tf
@@ -6,22 +6,12 @@ output "Pacman" {
   value = format("http://%s", google_compute_global_address.pacman.address)
 }
 
-/*output "REST_Proxy" {
-  value = var.instance_count["rest_proxy"] >= 1 ? join(
-    ",",
-    formatlist(
-      "http://%s",
-      google_compute_global_address.rest_proxy.*.address,
-    ),
-  ) : "REST Proxy has been disabled"
-}*/
-
 output "KSQL_Server" {
-  value = var.instance_count["ksql_server"] >= 1 ? join(
-    ",",
-    formatlist(
-      "http://%s",
-      google_compute_global_address.ksql_server.*.address,
-    ),
-  ) : "KSQL Server has been disabled"
+  value = var.instance_count["ksql_server"] >= 1 ? join(",", formatlist("http://%s", google_compute_global_address.ksql_server.*.address,),) : "KSQL Server has been disabled"
 }
+
+/************** Uncomment this if you need access to the REST Proxy endpoint **************
+output "REST_Proxy" {
+  value = var.instance_count["rest_proxy"] >= 1 ? join(",", formatlist("http://%s", google_compute_global_address.rest_proxy.*.address,),) : "REST Proxy has been disabled"
+}
+*/
