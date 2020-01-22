cd79fea8b2b38e40b3e5b7101e7936066d4e9343
Robin Moffatt <robin@moffatt.me>
2020-01-22 11:17:22 +0000

    Updated for blog article

--- sonos/log-sonos-to-kafka.sh
+++ sonos/log-sonos-to-kafka.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-set +x
+set -x
 
-source .env
-
-curl -s 'http://192.168.10.140:1400/support/review' | \
-    xq -c '.ZPNetworkInfo.ZPSupportInfo[] | {ZPInfo: .ZPInfo.ZoneName, data: [(.File[] | select (."@name" == "/proc/ath_rincon/status") | ."#text" | split("\n")[] | select((. | contains("OFDM")) or (.|contains("Noise")) or (.|contains("PHY"))))], rawdata: (.File[] | select (."@name" == "/proc/ath_rincon/status") | ."#text")}' | \
-    docker run --interactive --rm edenhill/kafkacat:1.5.0 \
-        -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN \
-        -X ssl.ca.location=./etc/ssl/cert.pem -X api.version.request=true \
-        -b ${CCLOUD_BROKER_HOST}:9092 \
-        -X sasl.username="${CCLOUD_API_KEY}" \
-        -X sasl.password="${CCLOUD_API_SECRET}" \
-        -t sonos-metrics -P
+while [ 1 -eq 1 ];do 
+    curl -s 'http://192.168.10.98:1400/support/review' | \
+        xq -c '.ZPNetworkInfo.ZPSupportInfo[] | 
+            {ZPInfo: .ZPInfo.ZoneName, 
+            data: [(.File[] | 
+                    select (."@name" == "/proc/ath_rincon/status") | 
+                    ."#text" | 
+                    split("\n")[] | 
+                    select((. | contains("OFDM")) or (.|contains("Noise")) or (.|contains("PHY"))))]
+                }' | \
+            docker exec -i kafkacat kafkacat \
+                -b kafka-1:39092,kafka-2:49092,kafka-3:59092 \
+                -t sonos-metrics -P -T | jq '.'
+    sleep 20
+done
