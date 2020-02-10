da35530a49c2590e6ac0b7e79bf1de7758e3eaf2
Robin Moffatt <robin@moffatt.me>
2020-02-10 16:23:18 +0000

    Clear out old stuff

--- build-a-streaming-pipeline/scripts/create-dynamic-mapping.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -s -XPUT "http://elasticsearch:9200/_template/kafkaconnect/" -H 'Content-Type: application/json' -d'
-          {
-            "template": "*",
-            "settings": { "number_of_shards": 1, "number_of_replicas": 0 },
-            "mappings": { "dynamic_templates": [ { "dates": { "match": "*_TS", "mapping": { "type": "date" } } } ]  }
-          }'
