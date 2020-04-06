3cd0bd331588f4f32f950e513f900f397238709c
Robin Moffatt <robin@rmoff.net>
2020-04-06 21:44:20 +0100

    add local

---
+++ wifi-fun/latest_timestamp_local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Kafka:"
+
+kafkacat -b localhost:9092 \
+    -C -c1 -o -1 -t pcap -u |jq '.timestamp'|sed -e 's/"//g' | sed 's/[0-9][0-9][0-9]$//g' | xargs -Ifoo date -j -f %s foo
+
+echo "Elasticsearch:"
+
+curl -s http://localhost:9200/pcap/_search \
+    -H 'content-type: application/json' \
+    -d '{ "size": 1, "sort": [ { "timestamp": { "order": "desc" } } ] }' |\
+    jq '.hits.hits[]._source.timestamp'|sed -e 's/"//g' | sed 's/[0-9][0-9][0-9]$//g' | xargs -Ifoo date -j -f %s foo
