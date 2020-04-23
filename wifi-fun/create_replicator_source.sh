0bda0ac9541a541a924befc61ac3382a1516995a
Robin Moffatt <robin@rmoff.net>
2020-04-23 12:53:04 +0100

    Add telegram bot code and blog article

--- wifi-fun/create_replicator_source.sh
+++ wifi-fun/create_replicator_source.sh
@@ -21,18 +21,18 @@ curl -s -X PUT -H "Accept:application/json" \
         "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
         "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
         "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
-        "src.kafka.bootstrap.servers": "'$CCLOUD_BROKER_HOST':9092",
+        "src.kafka.bootstrap.servers": "'$CCLOUD_BROKER_HOST'",
         "src.kafka.security.protocol": "SASL_SSL",
         "src.kafka.sasl.mechanism": "PLAIN",
         "src.kafka.sasl.jaas.config": "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"'$CCLOUD_API_KEY'\" password=\"'$CCLOUD_API_SECRET'\";",
         "src.consumer.group.id": "replicator-'$epoch'",
+    	"src.kafka.auto.offset.reset": "latest",
         "dest.kafka.bootstrap.servers": "kafka-1:39092,kafka-2:49092,kafka-3:59092",
         "topic.whitelist": "pcap",
-        "topic.rename.format":"${topic}",
+        "topic.rename.format":"${topic}-ccloud-'$epoch'",
         "confluent.license":"",
         "confluent.topic.bootstrap.servers":"kafka-1:39092,kafka-2:49092,kafka-3:59092",
         "confluent.topic.replication.factor":1,
         "offset.start":"consumer"
         }' | jq '.'
 
-#         "topic.rename.format":"${topic}-ccloud-'$epoch'",
