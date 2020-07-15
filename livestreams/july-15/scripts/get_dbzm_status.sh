ddbc2b91b77848f868f1e9ff1951d6a5b36faa10
Viktor Gamov <viktor@confluent.io>
2020-07-15 15:07:44 -0400

    Adding demo for July 15th livestream (#133)

---
+++ livestreams/july-15/scripts/get_dbzm_status.sh
@@ -0,0 +1,1 @@
+curl -s "http://localhost:8083/connectors"| jq '.[]'| xargs -I{connector_name} curl -s "http://localhost:8083/connectors/"{connector_name}"/status"| jq -c -M '[.name,.connector.state,.tasks[].state]|join(":|:")'
