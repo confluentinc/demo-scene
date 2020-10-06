65ba048ab92bef17921132bac6ee5c1120521be0
Viktor Gamov <viktor@confluent.io>
2020-10-06 14:24:45 -0400

    Added code from oct-6 livestreams (#157)

---
+++ livestreams/october-6/websockets-ksqldb/src/test/java/io/confluent/developer/websocketsksqldb/WebsocketsKsqldbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.websocketsksqldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsKsqldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
