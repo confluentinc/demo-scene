0d04c133a8bd15b61921a55a7451338daddc321b
Viktor Gamov <viktor@confluent.io>
2020-12-04 17:12:03 -0500

    Spring Cloud Streams demo (#165)
    * A small example demonstration usage of spring-cloud-stream apis
    
    - producing data to kafka (using kafka binder)
    - consuming data from kafka (kafka binder)
    - processing data (word count) using Kafka Streams
    
    * Added example of ccloud config

---
+++ wordcount-spring-cloud-stream/src/test/java/io/confluent/developer/springcloudstreams/SpringCloudStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.springcloudstreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
