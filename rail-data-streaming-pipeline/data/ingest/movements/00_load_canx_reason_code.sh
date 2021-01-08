ba65a32ee1ed951d6d4408451c9d0f9a57e579a7
Robin Moffatt <robin@rmoff.net>
2021-01-08 11:59:55 +0000

    Update cancellation reason codes

--- rail-data-streaming-pipeline/data/ingest/movements/00_load_canx_reason_code.sh
+++ rail-data-streaming-pipeline/data/ingest/movements/00_load_canx_reason_code.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# See https://wiki.openraildata.com/index.php?title=Delay_Attribution_Guide for latest data
 
 docker exec kafkacat \
   kafkacat -b broker:29092 -t canx_reason_code -P -K: -l /data/ingest/movements/canx_reason_code.dat
