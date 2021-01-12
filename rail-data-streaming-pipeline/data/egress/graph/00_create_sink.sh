33cd5f36600a5d421b7bbfb16c0725afd5bc6369
Robin Moffatt <robin@rmoff.net>
2021-01-12 09:44:14 +0000

    Update neo4j cypher

--- rail-data-streaming-pipeline/data/egress/graph/00_create_sink.sh
+++ rail-data-streaming-pipeline/data/egress/graph/00_create_sink.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 curl -i -X PUT -H "Accept:application/json" \
-    -H  "Content-Type:application/json" http://localhost:28083/connectors/sink-neo4j-train-cancellations/config \
+    -H  "Content-Type:application/json" http://localhost:8083/connectors/sink-neo4j-train-cancellations-v05/config \
     -d '{
     "connector.class": "streams.kafka.connect.sink.Neo4jSinkConnector",
-    "topics": "TRAIN_CANCELLATIONS_02",
+    "topics": "TRAIN_CANCELLATIONS",
     "key.converter":"org.apache.kafka.connect.storage.StringConverter",
-    "errors.tolerance": "all",
-    "errors.deadletterqueue.topic.name": "sink-neo4j-train-00_dlq",
+    "errors.tolerance": "none",
+    "errors.deadletterqueue.topic.name": "sink-neo4j-train-v05_dlq",
     "errors.deadletterqueue.topic.replication.factor": 1,
     "errors.deadletterqueue.context.headers.enable":true,
+    "kafka.bootstrap.servers":"broker:29092",
     "neo4j.server.uri": "bolt://neo4j:7687",
     "neo4j.authentication.basic.username": "neo4j",
     "neo4j.authentication.basic.password": "connect",
-    "neo4j.topic.cypher.TRAIN_CANCELLATIONS_02": "MERGE (train:train{id: event.TRAIN_ID}) MERGE (toc:toc{toc: event.TOC}) MERGE (canx_reason:canx_reason{reason: event.CANX_REASON}) MERGE (canx_loc:canx_loc{location: coalesce(event.CANCELLATION_LOCATION,\'<unknown>\')}) MERGE (train)-[:OPERATED_BY]->(toc) MERGE (canx_loc)<-[:CANCELLED_AT{reason:event.CANX_REASON, time:event.CANX_TIMESTAMP}]-(train)-[:CANCELLED_BECAUSE]->(canx_reason)"
+    "neo4j.topic.cypher.TRAIN_CANCELLATIONS": "MERGE (train:train{id: coalesce(event.TRAIN_ID,'\''<unknown>'\''), category: coalesce(event.TRAIN_CATEGORY,'\''<unknown>'\''), power: coalesce(event.POWER_TYPE,'\''<unknown>'\'')}) MERGE (toc:toc{toc: coalesce(event.TOC,'\''<unknown>'\'')}) MERGE (canx_reason:canx_reason{reason: coalesce(event.CANX_REASON,'\''<unknown>'\'')}) MERGE (canx_loc:canx_loc{location: coalesce(event.CANCELLATION_LOCATION,'\''<unknown>'\'')}) MERGE (train)-[:OPERATED_BY]->(toc) MERGE (canx_loc)<-[:CANCELLED_AT{reason:event.CANX_REASON, time:event.CANX_TIMESTAMP}]-(train)-[:CANCELLED_BECAUSE]->(canx_reason)"
        } '
 
