f298d2983e7b6ee5554694387692a035ed2d1b72
Viktor Gamov <viktor@confluent.io>
2021-02-02 00:04:33 -0500

    Added SCS demo app from jan 26th stream (#194)

---
+++ livestreams/january-26-2021/scs-kafka/src/test/java/io/confluent/developer/scslivestreams/ScsLivestreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.scslivestreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsLivestreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
