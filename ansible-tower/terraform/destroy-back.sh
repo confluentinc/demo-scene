6d302f72b4cf9ffec3f4a6e9104a022f684cf610
Domenic Bove <domenicbove@gmail.com>
2021-04-08 14:11:23 -0400

    corrected destroy script

--- ansible-tower/terraform/destroy-back.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-shopt -s expand_aliases
-alias terraform="docker run -it --rm -v '$(pwd):$(pwd)' -w '$(pwd)' hashicorp/terraform:light"
-
-terraform destroy --auto-approve
-
-unalias terraform
--- ansible-tower/terraform/destroy.sh
+++ ansible-tower/terraform/destroy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-shopt -s expand_aliases
-alias tf="docker run -it --rm -v '$(pwd):$(pwd)' -w '$(pwd)' hashicorp/terraform:light"
-
-type -a tf
-
-tf destroy --auto-approve
-
-unalias tf
+terraform destroy --auto-approve
