782ad18b3e91568958f2f63abfd075e7680d096b
Viktor Gamov <viktor@confluent.io>
2021-05-03 23:06:59 -0400

    Initial commit of for spring-kafka course on Confluent Developer

---
+++ spring-kafka-ccloud-course/src/test/java/io/confluent/developer/spring/SpringCcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
