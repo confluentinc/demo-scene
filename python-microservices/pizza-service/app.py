a837ba216067b335d05356f8891db7c1c332de69
Dave Klein <daveklein@usa.net>
2022-08-30 10:31:20 -0700

    updating python code for blog post

--- python-microservices/pizza-service/app.py
+++ python-microservices/pizza-service/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import json
 import pizza_service
 
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 @app.route('/order/<count>', methods=['POST'])
 def order_pizzas(count):
     order_num = pizza_service.order_pizzas(int(count))
-    return '{"order_id":"' + order_num + '"}'
+    return json.dumps({"order_id": order_num})
 
 
 @app.route('/order/<order_id>', methods=['GET'])
